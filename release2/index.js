e92ad46c4111ed80f3b13490138a0e513d6a8e5c
saideep0531 <saideepgudla@gmail.com>
2021-04-05 22:26:13 +0530

    release 3

--- release2/index.js
+++ release2/index.js
@@ -5,18 +5,18 @@
            'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
            date_data = new Date();
-           //Get day
+  //Get day
            current_day = date_data.getDay()
            document.getElementById('day').innerHTML = `${days[current_day]}`
 
-           //Get Month date year
-           current_month = date_data.getMonth()
-           current_date = date_data.getDate()
-           current_year = date_data.getFullYear()
-           document.getElementById('month_date_year').innerHTML = 
-           `${months[current_month]} ${current_date}, ${current_year}`
+   //Get Month date year
+  current_month = date_data.getMonth()
+current_date = date_data.getDate()
+ current_year = date_data.getFullYear()
+document.getElementById('month_date_year').innerHTML = 
+`${months[current_month]} ${current_date}, ${current_year}`
 
-           //Get time
+ //Get time
            current_hour = date_data.getHours()
            current_minute = date_data.getMinutes()
            am_pm = ''
@@ -24,12 +24,12 @@
                 //It's pm
                 current_hour = current_hour%12;
              am_pm = 'PM'
-                //document.body.background = "night.jpg"; 
+ //document.body.background = "night.jpg"; 
             }
             else{
             //It's am
             am_pm = `AM`
-            //document.body.background = "morning.jpg";
+ //document.body.background = "morning.jpg";
             }
            document.getElementById('time').innerHTML = `${current_hour}:${current_minute} ${am_pm}`
            
